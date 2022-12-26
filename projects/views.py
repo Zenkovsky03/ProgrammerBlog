e9684c20dfa01853a13022768430c9a9eb8889cd
Zenkovsky03 <mateuszzaniewski1@gmail.com>
2022-12-26 13:32:23 +0100

    Rendering simpple data to tempates using routing

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+projectList = [
+  {
+    'id' : '1',
+    'tittle' : 'Ecommerce Website',
+    'description' : 'Fully ecommercial website'
+  },
+  {
+    'id' : '2',
+    'tittle' : 'Portfolio website',
+    'description' : 'This is project where i build out my portfolio'
+  },
+  {
+    'id' : '3',
+    'tittle' : 'Social network',
+    'description' : 'Open source project avaiable now!'
+  }
+]
+
 def projects(request):
-  return render(request, 'projects/projects.html')
+  page = "projects"
+  number = 10
+  context = {
+    'page' : page,
+    'number' : number,
+    'projects' : projectList
+  }
+  return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-  return render(request, 'projects/single-project.html')
+  projectObj = None
+  for i in projectList:
+    if i['id'] == pk:
+      projectObj = i
+  return render(request, 'projects/single-project.html', {'project' : projectObj})
